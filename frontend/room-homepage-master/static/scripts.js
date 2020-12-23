75e708945ab97b939fdde2bf64f50066aa5006f1
Pedro Isac <ss.pedroisac@gmail.com>
2020-12-23 09:56:59 -0300

    Bug fixes

--- frontend/room-homepage-master/static/scripts.js
+++ frontend/room-homepage-master/static/scripts.js
@@ -10,22 +10,19 @@ function slideController(n) {
   const titleSlide = document.getElementById("title-slide");
   const textSlide = document.getElementById("text-slide");
   if (sliderCount + n === 1) {
-    navSection.style.backgroundImage =
-      "url(../images/desktop-image-hero-1.jpg)";
+    navSection.style.backgroundImage = "url(images/desktop-image-hero-1.jpg)";
     titleSlide.innerHTML = "Discover innovative ways to decorate";
     textSlide.innerHTML = `We provide unmatched quality, comfort, and style for property owners across the country.
       Our experts combine form and function in bringing your vision to life. Create a room in your
       own style with our collection and make your property a reflection of you and what you love.`;
   } else if (sliderCount + n === 2) {
-    navSection.style.backgroundImage =
-      "url(../images/desktop-image-hero-2.jpg)";
+    navSection.style.backgroundImage = "url(images/desktop-image-hero-2.jpg)";
     titleSlide.innerHTML = "We are available all across the globe";
     textSlide.innerHTML = `With stores all over the world, it's easy for you to find furniture for your home or place of business.
     Locally, we’re in most major cities throughout the country. Find the branch nearest you using our
     store locator. Any questions? Don't hesitate to contact us today.`;
   } else if (sliderCount + n === 3) {
-    navSection.style.backgroundImage =
-      "url(../images/desktop-image-hero-3.jpg)";
+    navSection.style.backgroundImage = "url(images/desktop-image-hero-3.jpg)";
     titleSlide.innerHTML = "Manufactured with the best materials";
     textSlide.innerHTML = `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology
     to ensure that every product is made as perfect and as consistent as possible. With three decades of
