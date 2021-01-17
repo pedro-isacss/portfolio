2dd3beaa9ca3971cec5e77d684eae2a51eea7390
Pedro Isac <ss.pedroisac@gmail.com>
2021-01-17 11:16:20 -0300

    Add iPay animations

--- frontend/ipay/scripts.js
+++ frontend/ipay/scripts.js
@@ -1,56 +1,45 @@
+const heightWindow = window.innerHeight;
+let position = 0;
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section3 = document.getElementById("section3");
+const section4 = document.getElementById("section4");
+const section5 = document.getElementById("section5");
+const section6 = document.getElementById("section6");
+const section7 = document.getElementById("section7");
+const section8 = document.getElementById("section8");
+const sections = [
+  section2,
+  section3,
+  section4,
+  section5,
+  section6,
+  section7,
+  section8,
+];
 window.addEventListener("scroll", function () {
-  const heightWindow = window.innerHeight;
-  if (
-    window.pageYOffset > heightWindow / 3 &&
-    window.pageYOffset < heightWindow
-  ) {
-    const section = document.getElementById("section2");
-    section.setAttribute("class", "animated know-more-container");
-    section.style.opacity = 1;
-  } else if (
-    window.pageYOffset > (heightWindow * 2) / 3 &&
-    window.pageYOffset < heightWindow * 2
-  ) {
-    const section = document.getElementById("section3");
-    section.setAttribute(
-      "class",
-      "animated know-more-container bg-orange-light"
-    );
-    section.style.opacity = 1;
-  } else if (
-    window.pageYOffset > (heightWindow * 3) / 3 &&
-    window.pageYOffset < heightWindow * 3
-  ) {
-    const section = document.getElementById("section4");
-    section.setAttribute("class", "animated know-more-container");
-    section.style.opacity = 1;
-  } else if (
-    window.pageYOffset > (heightWindow * 4) / 3 &&
-    window.pageYOffset < heightWindow * 4
-  ) {
-    const section = document.getElementById("section5");
-    section.setAttribute("class", "animated computer-phone-container");
-    section.style.opacity = 1;
-  } else if (
-    window.pageYOffset > (heightWindow * 5) / 3 &&
-    window.pageYOffset < heightWindow * 5
-  ) {
-    const section = document.getElementById("section6");
-    section.setAttribute("class", "animated business-container");
-    section.style.opacity = 1;
-  } else if (
-    window.pageYOffset > (heightWindow * 6) / 3 &&
-    window.pageYOffset < heightWindow * 6
-  ) {
-    const section = document.getElementById("section7");
-    section.setAttribute("class", "animated card-container bg-orange-light");
-    section.style.opacity = 1;
-  } else if (
-    window.pageYOffset > (heightWindow * 7) / 3 &&
-    window.pageYOffset < heightWindow * 7
-  ) {
-    const section = document.getElementById("section8");
-    section.setAttribute("class", "animated");
-    section.style.opacity = 1;
-  }
+  sections.forEach((section) => {
+    if (window.pageYOffset > position && section !== section1) {
+      if (
+        section.offsetTop >= window.pageYOffset &&
+        section.offsetTop <= window.pageYOffset + heightWindow
+      ) {
+        section.classList.add("animated");
+      } else {
+        section.classList.remove("animated");
+      }
+      console.log("Down");
+    } else if (window.pageYOffset <= position && section !== section8) {
+      if (
+        section.offsetTop + heightWindow >= window.pageYOffset &&
+        section.offsetTop + heightWindow <= window.pageYOffset + heightWindow
+      ) {
+        section.classList.add("animated-up");
+      } else {
+        section.classList.remove("animated-up");
+      }
+      console.log("Up");
+    }
+  });
+  position = window.pageYOffset;
 });
